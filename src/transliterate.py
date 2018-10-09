78ac3f660bea32dbb84107c6e7d6c6e7abf23d24
vukbatanovic <vukbatanovic@users.noreply.github.com>
2018-10-10 01:45:12 +0200

    Upload of documents transliterated into the Latin script

---
+++ src/transliterate.py
@@ -0,0 +1,16 @@
+import os
+import cyrtranslit
+
+def transliterate_documents():
+    doc_collection_path = '../data/documents'
+    file_paths = os.listdir(doc_collection_path)
+    file_paths = [doc_collection_path + '/' + doc_path for doc_path in file_paths if doc_path.endswith('.txt')]
+    for input_path in file_paths:
+        output_path = input_path + '.lat'
+        with open(input_path, 'r', encoding='utf-8') as input_file:
+            with open(output_path, 'w', encoding='utf-8') as output_file:
+                for line in input_file:
+                    lat_line = cyrtranslit.to_latin(line)
+                    output_file.write(lat_line)
+
+transliterate_documents()
